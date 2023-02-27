a421a9c9d310d20fa4ac32ab7aa21e39679c5606
WunderWungiel <89900851+WunderWungiel@users.noreply.github.com>
2023-02-27 16:49:56 +0100

    Help update
    I have also forgotten to mention introduced progress bar :)

--- functions/help_f.py
+++ functions/help_f.py
@@ -19,6 +19,8 @@ def help_f():
 
  {Fore.LIGHTGREEN_EX}--content {Style.RESET_ALL}{Fore.BLUE}<min_id> <max_id>{Style.RESET_ALL} - download using apps ID (loop through range of IDs you've provided. {Fore.RED}Very slow and hungry!{Style.RESET_ALL} If you import one ID, it will download one app with this ID.
 
+ {Fore.LIGHTGREEN_EX}--filename {Style.RESET_ALL}{Fore.BLUE}<filename>{Style.RESET_ALL} - download file with provided filename.
+
  {Fore.LIGHTGREEN_EX}--removedm {Style.RESET_ALL}{Fore.BLUE}<directory / filename>{Style.RESET_ALL} - removes DM from files (sisx.dm, sis.dm, 3gp.dm, mp4.dm, gif.dm).
 
  Wayback Machine often refuses connection after many requests. You will be notified about such case with a bunch of errors, you need to retry then.
